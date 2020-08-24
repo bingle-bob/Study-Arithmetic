9d39433a6a3d759872d6ad12c27971e567d9774c
moda <yangxuan_321@163.com>
2020-08-24 14:23:32 +0800

    GitTest

--- study/src/main/java/com/bob/study/AlgorithmProblemDome1.java
+++ study/src/main/java/com/bob/study/AlgorithmProblemDome1.java
@@ -1,11 +1,15 @@
 package com.bob.study;
 
 /**
- *
- */
+  * @Description: TODO  算法题 1.题目：古典问题：有一对兔子，从出生后第3个月起每个月都生一对兔子，小兔子长到第三个月后每个月又生一对兔子，假如兔子都不死，问每个月的兔子总数为多少？
+  * @author bob
+  * @date 2020/8/24 14:13
+  */
 
 
 public class AlgorithmProblemDome1 {
 
+    public static void main(String[] args) {
 
+    }
 }
